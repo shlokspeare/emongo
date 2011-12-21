fa70449089829bd86c356e3377fabb2d20cec7a2
Jeremy Hood <jeremy.hood@ericsson.com>
2011-12-21 15:08:30 -0500

    add include check

--- include/emongo_public.hrl
+++ include/emongo_public.hrl
@@ -1,5 +1,10 @@
+-ifndef(EMONGO_PUBLIC).
+
 % Additional options that can be passed to emongo:find()
 -define(TAILABLE_CURSOR, 2).
 -define(SLAVE_OK, 4).
 -define(OPLOG, 8).
 -define(NO_CURSOR_TIMEOUT, 16).
+
+-define(EMONGO_PUBLIC, true).
+-endif.
